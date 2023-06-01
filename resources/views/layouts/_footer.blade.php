38060efac81995e6f45bbb13828560bd516a3ff9
zack999 <827167689@qq.com>
2023-06-01 14:18:19 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="#">
+  <a href="https://www.laobia.online/" target=_blank class="text-decoration-none">
+    刻意练习，每日精进
+  </a>
+
+  <div class="float-end">
+    <a href="/about" class="text-decoration-none">关于</a>
+  </div>
+</footer>
