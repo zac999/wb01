681eabd814922b84d76fabff2e7369aa2e7b3ace
zack999 <827167689@qq.com>
2023-06-01 14:27:29 +0800

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -10,7 +10,7 @@
     但愿人长久，千里共婵娟。
   </p>
   <p>
-    <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+    <a class="btn btn-lg btn-success" href="{{ route('signup')}}" role="button">现在注册</a>
   </p>
 </div>
 @stop
