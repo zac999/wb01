0f860661cf24dce0b9430d4a36d1adac521961ca
zack999 <827167689@qq.com>
2023-06-01 14:04:45 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,11 +2,25 @@
 <html>
 
 <head>
-  <title>@yield('title', 'Weibo App') - Laravel 新手加油</title>
+  <title>@yield('title', 'Wb01 App') - Laravel 入门学习</title>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 
 <body>
-  @yield('content')
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="/">Wb01 App</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    @yield('content')
+  </div>
 </body>
 
 </html>
